991fcd837fbd856c38bd5b8a5e57d663c298f304
Shashankp122003 <94059030+Shashankp122003@users.noreply.github.com>
2023-11-16 21:11:58 +0530

    Create dmem.v

---
+++ Design_code/dmem.v
@@ -0,0 +1,8 @@
+module dmem(input wire clk, we,
+	input wire [31:0] a, wd,
+	output wire [31:0] rd);
+	reg [31:0] RAM[63:0];
+assign rd = RAM[a[31:2]]; // word aligned
+	always @(posedge clk)
+	if (we) RAM[a[31:2]] <= wd;
+endmodule
