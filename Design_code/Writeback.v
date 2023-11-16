464f91cd7e9d6d443716de047009254f99f4a0c9
Shashankp122003 <94059030+Shashankp122003@users.noreply.github.com>
2023-11-16 21:13:13 +0530

    Create Writeback.v

---
+++ Design_code/Writeback.v
@@ -0,0 +1,12 @@
+module regfile(input wire clk,
+	input wire we3,
+	input wire [5:0] a1, a2, a3,
+	input wire [31:0] wd3,
+	output wire [31:0] rd1, rd2);
+	reg [31:0] rf[31:0];
+
+	always @(posedge clk)
+		if (we3) rf[a3] <= wd3;
+	assign rd1 = (a1 != 0) ? rf[a1] : 0;
+	assign rd2 = (a2 != 0) ? rf[a2] : 0;
+endmodule
